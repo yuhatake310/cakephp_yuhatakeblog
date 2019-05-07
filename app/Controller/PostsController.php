c5fd77904ecc65edaafebf54eb372a6999f89143
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-05-08 00:00:36 +0900

    [add]Postsコントローラーにaddアクション追加
    新しい記事を投稿できるようにするためのビジネスロジック

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -17,4 +17,15 @@ class PostsController extends AppController {
 		}
 		$this->set('post', $post);
 	}
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Post->create();
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success(__('Your post has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Flash->error(__('Unable to add your post.'));
+		}
+	}
 }
