c5301e88ba2682e2e6b4104b8d4d4a6621b442d3
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-05-29 20:35:25 +0900

    [update]投稿を新規作成する際に、現在ログイン中のユーザー保存する様に修正

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -21,6 +21,7 @@ class PostsController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Post->create();
+			$this->request->data['Post']['user_id'] = $this->Auth->user('id');
 			if ($this->Post->save($this->request->data)) {
 				$this->Flash->success(__('Your post has been saved.'));
 				return $this->redirect(array('action' => 'index'));
