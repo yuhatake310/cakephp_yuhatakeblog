24a97db9e095c9c475daaf1511f1bada18c782a9
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-05-29 16:37:11 +0900

    [add]BlowfishPasswordHasherを追加。

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,6 +1,9 @@
 <?php
+App::uses('AppModel', 'Model');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
+
 class User extends AppModel {
-	piblic $validate = array(
+	public $validate = array(
 		'username' => array(
 			'required' => array(
 				'rule' => 'notBlank',
@@ -21,4 +24,14 @@ class User extends AppModel {
 			)
 		)
 	);
+
+	public function beforeSave($options = array()) {
+	if (isset($this->data[$this->alias]['password'])) {
+		$passwordHasher = new BlowfishPasswordHasher();
+		$this->data[$this->alias]['password'] = $passwordHasher->hash(
+			$this->data[$this->alias]['password']
+		);
+	}
+	return true;
+	}
 }
