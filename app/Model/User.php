8ed20e469ed69869ab46f4c58aed7d4bd6ee0714
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-05-31 17:53:14 +0900

    [update]インデントミス修正

--- app/Model/User.php
+++ app/Model/User.php
@@ -40,12 +40,12 @@ class User extends AppModel {
 	);
 
 	public function beforeSave($options = array()) {
-	if (isset($this->data[$this->alias]['password'])) {
-		$passwordHasher = new BlowfishPasswordHasher();
-		$this->data[$this->alias]['password'] = $passwordHasher->hash(
-			$this->data[$this->alias]['password']
-		);
-	}
-	return true;
+		if (isset($this->data[$this->alias]['password'])) {
+			$passwordHasher = new BlowfishPasswordHasher();
+			$this->data[$this->alias]['password'] = $passwordHasher->hash(
+				$this->data[$this->alias]['password']
+			);
+		}
+		return true;
 	}
 }
