b492bbcd10173eb57ace481e4820f5b64e7dea41
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-13 14:58:44 +0900

    [update]Userモデルで利用するテーブル名を変更
    同じデータベース内で同じテーブル名を利用したいため

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,6 +3,8 @@ App::uses('AppModel', 'Model');
 App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel {
+	public $useTable = 'cake_blog_users';
+
 	public $validate = array(
 		'username' => array(
 			'required' => array(
